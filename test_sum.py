49228ce5db2113af6b3502d4536237c358167fb4
gtrg55 <jisser@yandex.ru>
2022-10-09 18:50:56 +0300

    Add files via upload

---
+++ test_sum.py
@@ -0,0 +1,52 @@
+
+import pytest
+
+import time
+import math
+from statistics import mean
+time_start = time.time()
+mas = [1,2,3,4,5,6,7,11,222,31]
+
+
+print(mas)
+
+
+def maxx():
+    return max(mas)
+def minn():
+    return min(mas)
+
+def summ():
+    sum = 0
+
+    for i in range(len(mas)):
+        b = int(mas[i])
+        sum = sum + b
+    return sum
+def avg():
+    return round(mean(mas))
+
+
+def multt():
+    mult = 1
+    for i in range(len(mas)):
+        b = int(mas[i])
+        mult = mult * b
+
+    return mult
+
+
+print("Сумма", summ())
+print("Произведение", multt())
+print("Минимальное", minn())
+print("Максимальное", maxx())
+print("Среднее значение массива ", avg())
+time_end = time_start - time.time()
+print("Расчет занял", (abs(round(time_end, 5))), "секунд")
+def test_summ_good():
+    test_sum = 0
+    for i in range(len(mas)):
+        b = int(mas[i])
+        test_sum = test_sum + b
+    #print(test_sum)
+    assert summ() == test_sum
